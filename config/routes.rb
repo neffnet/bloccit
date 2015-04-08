31e179f4b204e0ecb3131cbcf5470b6a83e63dce
neffnet <neff@neffnet.net>
2015-04-08 14:44:07 -0500

    add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
-    root to: 'welcome#index'
+  resources :posts
+  get 'about' => 'welcome#about'
+  
+  root to: 'welcome#index'
 end
