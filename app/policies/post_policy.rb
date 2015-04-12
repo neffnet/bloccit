070000a48e555bfc0499d46817ac7317425a3601
neffnet <neff@neffnet.net>
2015-04-12 01:38:51 -0500

    complete authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,24 @@
 class PostPolicy < ApplicationPolicy
+
+  class Scope
+    attr_reader :user, :scope
+    def initialize(user,scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if !user.present?
+        scope.where('1=2')
+        # what's the "right" way to do that?
+      elsif user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(:user => user)
+      end
+    end
+  end
+
   def index?
     true
   end
