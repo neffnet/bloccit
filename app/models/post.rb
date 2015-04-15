6e9579cbc54272350e448e86fed4fe09bda03504
neffnet <neff@neffnet.net>
2015-04-15 17:45:58 -0500

    complete post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,26 @@ class Post < ActiveRecord::Base
   validates :body, length: {minimum:20},presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks:true,hard_wrap:true}
+    redcarpet = Redcarpet::Markdown.new(renderer,extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
+  def markdown_title
+    render_as_markdown self.title
+  end
+
+  def markdown_body
+    render_as_markdown self.body
+  end
+
+  private
+
+    def render_as_markdown(stuff)
+      markdown_to_html(stuff)
+    end
   
 end
