0454098a5b514cb48842bbfb1cf3766ecfab9004
neffnet <neff@neffnet.net>
2015-04-17 17:09:11 -0500

    adding comments to views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,22 @@
 class CommentsController < ApplicationController
   def new
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = Comment.new
       authorize @comment
   end
     def create
-      @post = Post.find(params[:id])
+      @post = Post.find(params[:post_id])
+      @topic = @post.topic
       @comment = current_user.comments.build(comment_params)
       @comment.post = @post
         authorize @comment
       if @comment.save
         flash[:notice] = "Comment added"
-        redirect_to @post
+        redirect_to [@topic,@post]
       else
         flash[:error] = "Sorry, there is an error"
-        redirect_to @post
+        redirect_to [@topic,@post]
       end
     end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  validates :body, presence: true
-  validates :post, presence: true
-  validates :user, presence: true
+  validates :body, length:{minimum:5}, presence: true
+  validates :user_id, presence: true
 
 end
