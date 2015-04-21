4241f36a26da6764328fd91dee53d649df7d66df
neffnet <neff@neffnet.net>
2015-04-21 13:58:36 -0500

    complete destroy assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,37 @@
 class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
     @comment = Comment.new
       authorize @comment
   end
+
     def create
       @post = Post.find(params[:post_id])
-      @topic = @post.topic
       @comment = current_user.comments.build(comment_params)
       @comment.post = @post
+      topic = @post.topic
         authorize @comment
       if @comment.save
         flash[:notice] = "Comment added"
-        redirect_to [@topic,@post]
+        redirect_to [topic,@post]
       else
         flash[:error] = "Sorry, there is an error: " + @comment.errors.full_messages.first
-        redirect_to [@topic,@post]
+        redirect_to [topic,@post]
       end
     end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
+    topic = @post.topic
     authorize @comment
 
     if @comment.destroy
       flash[:notice] = "Comment deleted"
-      redirect_to [@topic,@post]
+      redirect_to [topic,@post]
     else
       flash[:error] = "Error: failed to delete the comment"
-      redirect_to [@topic,@post]
+      redirect_to [topic,@post]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,19 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
 
+  # let's not nest so deeply
+  # resources :topics do
+  #   resources :posts, except: [:index] do
+  #     resources :comments, only: [:create,:destroy]
+  #   end
+  # end
+
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create,:destroy]
-    end
+    resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
   
   get 'about' => 'welcome#about'
