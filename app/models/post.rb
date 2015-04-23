2c031456763e6575d941bf6a6ec8300602432291
neffnet <neff@neffnet.net>
2015-04-22 19:30:04 -0500

    complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,8 @@ class Post < ActiveRecord::Base
 #  validates :topic, presence: true
 #  validates :user, presence: true
 
+  after_create :create_vote
+
   def up_votes
     votes.where(value:1).count
   end
@@ -31,5 +33,10 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank,new_rank)
   end
+
+  private
+    def create_vote
+      user.votes.create(post:self,value:1)
+    end
     
 end
