3c41d414aec5d73288ce45ee282207983d254f50
neffnet <neff@neffnet.net>
2015-04-24 15:41:42 -0500

    finish private topics checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
+      authorize @topic
+      authorize @post
   end
 
   def new
